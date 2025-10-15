e5d062afa4c99eeaf658a42d13d2101e025ec87a
Chan <cmadut@academic.rrc.ca>
2025-10-15 14:54:22 -0500

    Added validation for user form inputs.

--- script.js
+++ script.js
@@ -21,12 +21,40 @@ form.addEventListener("submit", (event) => {
 const validateForm = () => {
     let isValid = true;
 
-    // const emailInput = document.getElementById("email");
-    // const complexEmailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    // if (!complexEmailPattern.test(emailInput.value)) {
-    //     showInputError(emailInput, "Please enter a valid email address");
-    //     isValid = false;
-    // }
+    const employed = document.getElementById("employed");
+    const inquiry = document.getElementById("inquiry");
+    const applied = document.getElementById("applied");
+    const position = document.getElementById("position");
+    const startDate = document.getElementById("start-date");
+    const employmentStatusYes = document.getElementById("status-yes");
+    const employmentStatusChecked = document.querySelector('input[name = "employment-status"]:checked');
+    const statusInquiryChecked = document.querySelector('input[name = "status-inquiry"]:checked');
+    const appliedBeforeChecked = document.querySelector('input[name = "applied-before"]:checked');
+
+    if (position.value === "") {
+        showInputError(position, "You must select a position");
+        isValid = false;
+    }
+
+    if (startDate.value === "") {
+        showInputError(startDate, "You must provide a preferred start date");
+        isValid = false;
+    }
+
+    if (employmentStatusChecked == null) {
+        showInputError(employed, "One must be selected");
+        isValid = false;
+    }
+
+    if (employmentStatusYes.checked && statusInquiryChecked == null) {
+        showInputError(inquiry, "One must be selected");
+        isValid = false;
+    }
+
+    if (appliedBeforeChecked == null) {
+        showInputError(applied, "One must be selected");
+        isValid = false;
+    }
 
     return isValid;
 }
