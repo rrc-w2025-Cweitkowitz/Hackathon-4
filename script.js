546ceb301960b9d509ea47f643eda767ece72e53
Connor Weitkowitz <cweitkowitz@academic.rrc.ca>
2025-10-15 13:18:18 -0500

    Framework grabbed from demo to initlize form creation

---
+++ script.js
@@ -0,0 +1,59 @@
+
+const form = document.getElementById("userForm");
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const errorMessages = document.querySelectorAll(".error-message");
+    for (const el of errorMessages) {
+        el.remove();
+    }
+
+    //  Evaluate the return of the validateForm function
+    if (validateForm()) {
+        form.submit();
+        console.log("Validation successful");
+    } else {
+        console.log("Validation failed");
+    }
+
+
+})
+
+document.getElementById("username").addEventListener("change", (event) => {
+    console.log("Username has changed to: ", event.target.value);
+});
+
+
+const validateForm = () => {
+
+    let isValid = true;
+    const username = document.getElementById("username");
+
+    if (username.value === "") {
+        showInputError(username, "Username is required");
+        isValid = false;
+    }
+
+
+    const emailInput = document.getElementById("email");
+    const complexEmailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!complexEmailPattern.test(emailInput.value)) {
+        showInputError(emailInput, "Please enter a valid email address");
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+
+const showInputError = (inputElement, message) => {
+
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerText = message;
+    errorDisplay.className = "error-message";
+    errorDisplay.setAttribute("role", "alert");
+
+    inputElement.parentElement.appendChild(errorDisplay);
+}
